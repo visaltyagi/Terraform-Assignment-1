0b54c48533d7d33dbd36fe7b7e6e7123acd59e82
visaltyagi <107295081+visaltyagi@users.noreply.github.com>
2024-04-07 14:06:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+     region = "us-east-2"
+     access_key = "AKIAQRH4ND34WNGRNWOP"
+     secret_key = "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"
+}
+
+resource "aws_instance" "assignment-1" {
+       ami = "ami-0b4750268a88e78e0"
+       instance_type = "t2.micro"
+       key_name = "Terraform"
+       tags= {
+       Name = "assignment-1"
+       }
+}
